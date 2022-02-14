e4d7489526fe5ddd00287c91eb07c33680e6ec28
AVALON\benaz <benazir.rajabov@gmail.com>
2022-02-14 16:09:29 +0300

    Bug fixes and added random number generator and attempts counter

--- CleanetCode.TypesBranchesAndLoops/CleanetCode.GuessingNumberGame/Program.cs
+++ CleanetCode.TypesBranchesAndLoops/CleanetCode.GuessingNumberGame/Program.cs
@@ -1,14 +1,18 @@
 ﻿Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
 Console.WriteLine("Как вас зовут?");
-string userName = Console.ReadLine();
+string? userName = Console.ReadLine();
+int maxValue = 999;
 
 Console.WriteLine(
     $"Привет {userName}. " +
-    $"Я загадал тебе число от 0 до 99. " +
+    $"Я загадал тебе число от 0 до {maxValue}. " +
     $"Побробуй отгадать");
 
-int secretNumber = 42;
+Random rnd = new Random();
+int secretNumber = rnd.Next(maxValue);
 bool isWin = false;
+int counter = 0;
+
 
 while (!isWin)
 {
@@ -16,29 +20,40 @@ while (!isWin)
     bool isIntNumber = false;
     do
     {
-        Console.WriteLine("Введи число от 0 до 99:");
+        Console.WriteLine($"Введи число от 0 до {maxValue}:");
         string? userInput = Console.ReadLine();
         isIntNumber = int.TryParse(userInput, out userNumber);
+        counter++;
 
         if (!isIntNumber)
         {
-            Console.WriteLine($"Вы ввели {userInput}. Нужно ввести число от 0 до 99.");
+            Console.WriteLine($"Вы ввели {userInput}. Нужно ввести число от 0 до {maxValue}.");
+            continue;
         }
+        else if (userNumber < 0 || userNumber > maxValue)
+        {
+            Console.WriteLine($"Вы вышли за предел допустимого значения ({userNumber})");
+            continue;
+        }
+        else
+        {
+            if (userNumber > secretNumber)
+            {
+                Console.WriteLine($"Ваше число ({userNumber}) больше, чем загаданное.");
+            }
+            else if (userNumber < secretNumber)
+            {
+                Console.WriteLine($"Ваше число ({userNumber}) меньше, чем загаданное.");
+            }
+            else
+            {
+                isWin = true;
+                Console.WriteLine($"Поздравляю {userName}, вы победили! " +
+                    $"Количество попыток: {counter}");
+                break;
+            }
+        }
+
     } while (isIntNumber);
-
-    if (userNumber > secretNumber)
-    {
-        Console.WriteLine($"Ваше число ({userNumber}) больше, чем загаданное.");
-    }
-    else if (userNumber < secretNumber)
-    {
-        Console.WriteLine($"Ваше число ({userNumber}) меньше, чем загаданное.");
-    }
-    else
-    {
-        isWin = true;
-        Console.WriteLine("Вы победили!");
-    }
-
 }
 
